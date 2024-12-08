960135cf39c59bba1c176d18b6b1e7742a8760ba
Adhithya Srivatsan <addytrunks@gmail.com>
2024-12-08 14:38:09 +0530

    addy: remove deprecated speech-to-text function

--- sarvam_ai_speech.py
+++ sarvam_ai_speech.py
@@ -10,7 +10,7 @@ import json
 
 load_dotenv()
 
-def record_audio_to_base64(duration=5, samplerate=16000, channels=1):
+def record_audio_to_base64(duration=10, samplerate=16000, channels=1):
     """
     Records audio in real-time, converts it to base64-encoded string.
 
@@ -38,44 +38,6 @@ def record_audio_to_base64(duration=5, samplerate=16000, channels=1):
     # Encode buffer content to base64
     base64_audio = base64.b64encode(buffer.getvalue()).decode('utf-8')
     return base64_audio
-
-def speech_to_text(base64_audio,language_code="ta-IN"):
-    """
-    Sends a base64-encoded audio to the speech-to-text API and returns the transcribed text in the same language as the audio.
-    
-    Link: https://sarvam.ai/docs/speech-to-text
-
-    Parameters:
-    - base64_audio (str): Base64-encoded audio content.
-    - language_code (str): Language code for transcription (default is "ta-IN").
-        - Avalailable language codes: hi-IN, bn-IN, kn-IN, ml-IN, mr-IN, od-IN, pa-IN, ta-IN, te-IN, gu-IN
-    - with_timestamps (bool): Whether to include timestamps in the transcription (default is False).
-
-    Returns:
-    - str: Transcribed text or error message from the API.
-    """
-    
-    url = "https://api.sarvam.ai/speech-to-text"
-
-    data = {
-        "model": "saarika:v1",
-        "language_code": language_code,
-        "with_timestamps": "true"
-    }
-
-    files = {
-        "file": ("input.wav", base64.b64decode(base64_audio), "audio/wav")
-    }
-
-    headers = {
-    'api-subscription-key': os.getenv('SARVAM_API_KEY')
-    }
-
-    try:
-        response = requests.post(url, data=data, files=files,headers=headers)
-        return response.json()['transcript']
-    except requests.exceptions.RequestException as e:
-        return {"error": str(e)}
 
 def speech_to_text_translate(base64_audio):
     """
